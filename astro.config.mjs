22f963d84e4e7303dc6e4ccd6dfb43e144a07918
Erick <erickgp51@gmail.com>
2024-02-04 22:58:37 -0600

    Katze cup

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import auth from "auth-astro";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [auth()],
+  output: "server",
+  adapter: vercel()
+});
